769665dc10882b7e8f1af3057c82cead05a384bc
David Burles <dburles@gmail.com>
2015-08-20 15:51:57 +1000

    initial

---
+++ googlemaps-react.jsx
@@ -0,0 +1,60 @@
+App = React.createClass({
+  render() {
+    return <MyTestMap />;
+  }
+});
+
+MyTestMap = React.createClass({
+  mixins: [ReactMeteorData],
+  componentDidMount() {
+    GoogleMaps.load();
+  },
+  getMeteorData() {
+    return {
+      loaded: GoogleMaps.loaded(),
+      mapOptions: GoogleMaps.loaded() && this._mapOptions()
+    };
+  },
+  _mapOptions() {
+    return {
+      center: new google.maps.LatLng(-37.8136, 144.9631),
+      zoom: 8
+    };
+  },
+  render() {
+    if (this.data.loaded)
+      return <GoogleMap name="mymap" options={this.data.mapOptions} />;
+
+    return <div>Loading map...</div>;
+  }
+});
+
+GoogleMap = React.createClass({
+  propTypes: {
+    name: React.PropTypes.string.isRequired,
+    options: React.PropTypes.object.isRequired
+  },
+  componentDidMount() {
+    GoogleMaps.create({
+      name: this.props.name,
+      element: this.getDOMNode(),
+      options: this.props.options
+    });
+
+    GoogleMaps.ready(this.props.name, function(map) {
+      var marker = new google.maps.Marker({
+        position: map.options.center,
+        map: map.instance
+      });
+    });
+  },
+  render() {
+    return <div className="map-container"></div>;
+  }
+});
+
+if (Meteor.isClient) {
+  Meteor.startup(function() {
+    return React.render(<App />, document.getElementById('root'));
+  });
+}
