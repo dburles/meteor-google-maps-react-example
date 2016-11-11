77a6a81b5e855aac10e6f3923de0593b11d8f345
David Burles <dburles@gmail.com>
2016-11-11 11:54:15 +1100

    simplify

--- googlemaps-react.js
+++ googlemaps-react.js
@@ -2,14 +2,8 @@ import React, { PropTypes } from 'react';
 import { render } from 'react-dom';
 import MyMap from './imports/MyMap';
 
-class App extends React.Component {
-  render() {
-    return <MyMap />;
-  }
-}
-
 if (Meteor.isClient) {
   Meteor.startup(() => {
-    render(<App />, document.getElementById('root'));
+    render(<MyMap />, document.getElementById('root'));
   });
 }
--- imports/MyMap.js
+++ imports/MyMap.js
@@ -1,30 +1,28 @@
 import React from 'react';
 import GoogleMap from './lib/GoogleMap';
 
-class MyMap extends React.Component {
-  handleMapOptions() {
-    return {
-      center: new google.maps.LatLng(-37.8136, 144.9631),
-      zoom: 8,
-    };
-  }
+function handleMapOptions() {
+  return {
+    center: new google.maps.LatLng(-37.8136, 144.9631),
+    zoom: 8,
+  };
+}
 
-  handleOnReady(name) {
-    GoogleMaps.ready(name, map => {
-      const marker = new google.maps.Marker({
-        position: map.options.center,
-        map: map.instance,
-      });
+function handleOnReady(name) {
+  GoogleMaps.ready(name, map => {
+    const marker = new google.maps.Marker({
+      position: map.options.center,
+      map: map.instance,
     });
-  }
+  });
+}
 
-  render() {
-    return (
-      <GoogleMap onReady={this.handleOnReady} mapOptions={this.handleMapOptions}>
-        Loading!
-      </GoogleMap>
-    );
-  }
+function MyMap() {
+  return (
+    <GoogleMap onReady={handleOnReady} mapOptions={handleMapOptions}>
+      Loading!
+    </GoogleMap>
+  );
 }
 
 export default MyMap;
